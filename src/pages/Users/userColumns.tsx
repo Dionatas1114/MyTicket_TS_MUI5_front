ac55d28e021c59cda8bd8ea0dad3be2090b558e4
Diônatas Lazzari <sem.dionatas@gmail.com>
2023-01-04 23:24:17 -0300

    add headerTableSize constant vars

--- src/pages/Users/userColumns.tsx
+++ src/pages/Users/userColumns.tsx
@@ -1,9 +1,12 @@
 import { GridColDef } from '@mui/x-data-grid';
 import { i18n } from 'translate/i18n';
+import { headerTableSize } from 'utils/constants';
+
+const { small, medium, large, extraLarge } = headerTableSize;
 
 const data = {
-  fields: ['id', 'name', 'email', 'customer', 'profile', 'createdAt', 'updatedAt', 'actions'],
-  widths: [50, 180, 200, 80, 80, 160, 160, 200],
+  fields: ['id', 'name', 'email', 'profile', 'customer', 'createdAt', 'updatedAt', 'actions'],
+  widths: [small, extraLarge, extraLarge, medium, large, large, large, extraLarge],
   headerNames: (field: string) => {
     return i18n.t(`users.table.${field}`);
   },
---
+++ src/utils/constants/index.ts
@@ -0,0 +1,20 @@
+const heightScreenSize = {
+  small: 450,
+  medium: 700,
+  large: 1080,
+};
+
+const marginSize = {
+  small: 3,
+  medium: 6,
+  large: 12,
+};
+
+const headerTableSize = {
+  small: 50,
+  medium: 100,
+  large: 150,
+  extraLarge: 200,
+};
+
+export { heightScreenSize, marginSize, headerTableSize };
