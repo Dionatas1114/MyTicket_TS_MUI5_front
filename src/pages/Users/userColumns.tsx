311283af21e469d1669e3d42cfd502ee8ec6821c
Diônatas Lazzari <sem.dionatas@gmail.com>
2023-01-05 00:33:01 -0300

    simplify code

--- src/pages/Users/userColumns.tsx
+++ src/pages/Users/userColumns.tsx
@@ -6,31 +6,29 @@ import { getScreenWidth } from 'utils/functions/BrowserInfo';
 const { small, medium, large, extraLarge } = headerTableSize;
 const screenWidthSize = getScreenWidth();
 
-const data = {
-  fields: ['id', 'name', 'email', 'profile', 'customer', 'createdAt', 'updatedAt', 'actions'],
-  widths: [small, extraLarge, extraLarge, medium, large, large, large, extraLarge],
-  headerNames: (field: string) => {
-    return i18n.t(`users.table.${field}`);
-  },
+const fields = ['id', 'name', 'email', 'profile', 'customer', 'createdAt', 'updatedAt', 'actions'];
+const widths = [small, extraLarge, extraLarge, medium, large, large, large, extraLarge];
+const headerNames = (field: string) => {
+  return i18n.t(`users.table.${field}`);
 };
 
 if (screenWidthSize <= widthScreenSize.small) {
   //<= 1150px = remove positions 5 & 6
-  data.fields.splice(5, 2);
-  data.widths.splice(5, 2);
+  fields.splice(5, 2);
+  widths.splice(5, 2);
 } else if (screenWidthSize <= widthScreenSize.medium) {
   //<= 1400px = remove position 6
-  data.fields.splice(6, 1);
-  data.widths.splice(6, 1);
+  fields.splice(6, 1);
+  widths.splice(6, 1);
 }
 
 let columns = [];
 
-for (let i = 0; i < data.fields.length; i++) {
+for (let i = 0; i < fields.length; i++) {
   const column = {
-    field: data.fields[i],
-    headerName: data.headerNames(data.fields[i]),
-    width: data.widths[i],
+    field: fields[i],
+    headerName: headerNames(fields[i]),
+    width: widths[i],
     editable: true,
   };
 
