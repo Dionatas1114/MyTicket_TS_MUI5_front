fcfa12afbee2fd7b3364664029741835ecb0de19
Diônatas Lazzari <sem.dionatas@gmail.com>
2023-01-11 23:43:21 -0300

    add connection table

--- src/pages/Connections/index.tsx
+++ src/pages/Connections/index.tsx
@@ -1,18 +1,19 @@
-import { Container, Grid, Toolbar } from '@mui/material';
-import { CustomPaper } from 'components';
+import { Container, Toolbar } from '@mui/material';
+import { Copyright, CustomPaper, Title } from 'components';
+import { i18n } from 'translate/i18n';
+import ConnectionDataGrid from './connectionDataGrid';
 
 const Connections = () => {
   return (
-    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-      <Toolbar />
-      <Grid container spacing={3}>
-        Connections
-        <Grid item xs={12} md={8} lg={9}>
-          <CustomPaper sx={{ height: 240 }}>
-            <>{'<Connections />'}</>
-          </CustomPaper>
-        </Grid>
-      </Grid>
+    <Container maxWidth={false} sx={{ mt: 4, mb: 4 }}>
+      <CustomPaper>
+        <div>
+          <Title color="primary">{i18n.t('mainDrawer.listItems.connections')}</Title>
+          <Toolbar variant="dense" />
+          <ConnectionDataGrid />
+        </div>
+      </CustomPaper>
+      <Copyright />
     </Container>
   );
 };
