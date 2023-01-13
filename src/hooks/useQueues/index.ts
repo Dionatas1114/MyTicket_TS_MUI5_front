b3f7492bd0ed3ce6e2eff19f56bdacee6456d64c
Diônatas Lazzari <sem.dionatas@gmail.com>
2023-01-13 10:32:49 -0300

    add queue hook

---
+++ src/hooks/useQueues/index.ts
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+
+import { api } from 'services/api';
+import toastError from 'utils/toastError';
+import ConvertByTimeZone from 'utils/functions/ConvertByTimeZone';
+
+const useQueues = () => {
+  const [loading, setLoading] = useState(true);
+  const [hasMore, setHasMore] = useState(false);
+  const [queues, setQueues] = useState<Queue[]>([]);
+
+  useEffect(() => {
+    setLoading(true);
+    const delayDebounceFn = setTimeout(() => {
+      const fetchQueues = async () => {
+        try {
+          const { data } = await api.get('/queues');
+          console.log('🚀 ~ file: index.ts:18 ~ fetchQueues ~ data', data);
+          const queues = data?.map((queue: Queue) => {
+            return {
+              ...queue,
+              createdAt: ConvertByTimeZone(queue.createdAt),
+              updatedAt: ConvertByTimeZone(queue.updatedAt),
+            };
+          });
+
+          setQueues(queues);
+        } catch (err) {
+          setLoading(false);
+          toastError(err);
+        }
+      };
+      fetchQueues();
+    }, 500);
+    return () => clearTimeout(delayDebounceFn);
+  }, []);
+
+  return { queues, loading, hasMore };
+};
+
+export default useQueues;
