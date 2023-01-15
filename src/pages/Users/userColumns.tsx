9ef0fa5aa373519ef1d15dea0848926f273c4c91
Diônatas Lazzari <sem.dionatas@gmail.com>
2023-01-14 22:39:25 -0300

    update userColumns file

--- src/pages/Users/userColumns.tsx
+++ src/pages/Users/userColumns.tsx
@@ -1,11 +1,33 @@
-import * as React from 'react';
-import { GridColumns } from '@mui/x-data-grid';
-import { headerTableSize } from 'utils/constants';
+import { Save, Cancel, Edit, DeleteOutlined } from '@mui/icons-material';
+import {
+  GridActionsCellItem,
+  GridColumns,
+  GridRowId,
+  GridRowModes,
+  GridRowModesModel,
+} from '@mui/x-data-grid';
 import { i18n } from 'translate/i18n';
+import { headerTableSize } from 'utils/constants';
 
 const { medium, large, extraLarge } = headerTableSize;
 
-export default function getUserColumns(users: User[]) {
+export default function getUserColumns(
+  users: User[],
+  rowModesModel: GridRowModesModel,
+  handleSaveClick: (id: GridRowId) => () => void,
+  handleCancelClick: (id: GridRowId) => () => void,
+  handleEditClick: (id: GridRowId) => () => void,
+  handleDeleteClick: (id: GridRowId) => () => void
+) {
+  const editable: BooleanElements = {
+    name: true,
+    email: true,
+    profile: true,
+    customer: true,
+    createdAt: false,
+    updatedAt: false,
+  };
+
   const widths: NumberElements = {
     name: extraLarge,
     email: extraLarge,
@@ -13,7 +35,6 @@ export default function getUserColumns(users: User[]) {
     customer: medium,
     createdAt: large,
     updatedAt: large,
-    actions: medium,
   };
 
   const userKeys = Object.values(users).map((user) => {
@@ -25,7 +46,6 @@ export default function getUserColumns(users: User[]) {
       customer,
       createdAt,
       updatedAt,
-      actions: '',
     });
   });
 
@@ -37,10 +57,55 @@ export default function getUserColumns(users: User[]) {
       field: fields[i] || '',
       width: widths[fields[i]] || 0,
       headerName: i18n.t(`users.table.${fields[i]}`) || '',
+      editable: editable[fields[i]],
     };
 
     columns.push(column);
   }
+
+  const actionsColumn = {
+    field: 'actions',
+    type: 'actions',
+    headerName: i18n.t('users.table.actions'),
+    width: headerTableSize.medium,
+    getActions: ({ id }: User) => {
+      const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+
+      if (isInEditMode) {
+        return [
+          <GridActionsCellItem
+            icon={<Save />}
+            label="Save"
+            onClick={handleSaveClick(id)}
+            color="inherit"
+          />,
+          <GridActionsCellItem
+            icon={<Cancel />}
+            label="Cancel"
+            onClick={handleCancelClick(id)}
+            color="inherit"
+          />,
+        ];
+      }
+
+      return [
+        <GridActionsCellItem
+          icon={<Edit />}
+          label="Edit"
+          onClick={handleEditClick(id)}
+          color="inherit"
+        />,
+        <GridActionsCellItem
+          icon={<DeleteOutlined />}
+          label="Delete"
+          onClick={handleDeleteClick(id)}
+          color="inherit"
+        />,
+      ];
+    },
+  };
+
+  columns.push(actionsColumn);
 
   return columns;
 }
