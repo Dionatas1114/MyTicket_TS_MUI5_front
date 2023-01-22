60d7100e6d4c0be0a2e0e5b8f66aab4a0967ff4c
Diônatas Lazzari <sem.dionatas@gmail.com>
2023-01-22 10:28:07 -0300

    fix ChatPanel size

--- src/components/ChatPanel/Panels/SolvedPanel.tsx
+++ src/components/ChatPanel/Panels/SolvedPanel.tsx
@@ -6,14 +6,14 @@ import SelectInput, { SelectInputProps } from '../Inputs/Select';
 
 export default function SolvedPanel(props: SelectInputProps & SwitchInputProps & NewTicketProps) {
   return (
-    <Grid container spacing={2}>
+    <Grid container justifyContent="space-evenly" alignItems="center">
       <Grid item xs>
         <NewTicket {...props} />
       </Grid>
       <Grid item xs>
         <SwitchInput {...props} />
       </Grid>
-      <Grid item xs>
+      <Grid item>
         <SelectInput {...props} />
       </Grid>
     </Grid>
