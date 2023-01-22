c034d2f866b81d55a1cff12670bf0dbcac9fc743
Diônatas Lazzari <sem.dionatas@gmail.com>
2023-01-22 10:26:18 -0300

    refactor ChatPanel

--- src/components/ChatPanel/index.tsx
+++ src/components/ChatPanel/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Tabs, Tab, Box, Divider, SelectChangeEvent, styled, alpha } from '@mui/material';
+import { Tabs, Tab, Box, Divider, SelectChangeEvent } from '@mui/material';
 import { Search, CheckBox as Solved, MoveToInbox as Inbox } from '@mui/icons-material';
 import { i18n } from 'translate/i18n';
 
@@ -7,23 +7,7 @@ import InboxPanel from './Panels/InboxPanel';
 import SolvedPanel from './Panels/SolvedPanel';
 import SearchPanel from './Panels/SearchPanel';
 
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-
-const StyledPanelBox = styled(Box)(({ theme }) => ({
-  backgroundColor: alpha(theme.palette.common.black, 0.03),
-}));
-
-const TabPanel = ({ children, value, index, ...other }: TabPanelProps) => {
-  return (
-    <div role="tabpanel" hidden={value !== index} {...other}>
-      {value === index && <Box sx={{ py: 2 }}>{children}</Box>}
-    </div>
-  );
-};
+import { StyledPanelBox, TabPanel } from './TabPanel';
 
 export default function ChatPanel(contacts: Contact[]) {
   const [contact, setContact] = React.useState<Contact | null>(null);
@@ -34,8 +18,8 @@ export default function ChatPanel(contacts: Contact[]) {
   const [showAllTickets, setShowAllTickets] = React.useState(true);
 
   const handleChangeTabPanel = (_: React.SyntheticEvent, newValue: number) => setTabPanel(newValue);
-
   const handleLoading = (_: React.ChangeEvent, loading: boolean) => setLoading(loading);
+  const handleSelectQueue = (event: SelectChangeEvent) => setQueues(event.target.value as string);
 
   const handleShowAllTickets = (_: React.ChangeEvent, checked: boolean) =>
     setShowAllTickets(checked);
@@ -43,12 +27,10 @@ export default function ChatPanel(contacts: Contact[]) {
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) =>
     setSearch(event.target.value as string);
 
-  const handleSelectQueue = (event: SelectChangeEvent) => setQueues(event.target.value as string);
-
   const queueProps = { queues, handleSelectQueue };
-  const ticketProps = { showAllTickets, handleShowAllTickets };
   const loadingProps = { loading, handleLoading };
   const searchProps = { search, handleSearch };
+  const ticketProps = { showAllTickets, handleShowAllTickets };
   const contactProps = { contact, setContact, contacts: Object.values(contacts) };
 
   return (
