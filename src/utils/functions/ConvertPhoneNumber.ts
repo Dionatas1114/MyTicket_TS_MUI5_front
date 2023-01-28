ce944572bb3b5aedaf1145bd93a7c0ac39718582
Diônatas Lazzari <sem.dionatas@gmail.com>
2023-01-28 18:44:03 -0300

    upgrade phone function

--- src/utils/functions/ConvertPhoneNumber.ts
+++ src/utils/functions/ConvertPhoneNumber.ts
@@ -1,5 +1,7 @@
+import Masks from './Masks';
+
 export default function formatPhoneNumber(phoneNumber: string) {
   let prefix = '';
   phoneNumber.charAt(4) === '9' ? (prefix = '9') : prefix; // eslint-disable-line
-  return phoneNumber.replace(/^(\d{2})(\d{2})(\d{4})(\d{4})/, `($2) ${prefix}$3-$4`);
+  return Masks(phoneNumber, 'phone', prefix);
 }
